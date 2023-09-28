fc87437be3be0b21a4b970c6acf2ff5e3eaac933
Kinthali Saketh <103805011+kinthalisaketh@users.noreply.github.com>
2023-09-28 18:41:27 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Strings {
+    public static void main(String[] args){
+        String name=new String("Harry");
+        String nam ="Harry";
+        System.out.println(name);
+        System.out.println(nam);
+        Scanner sc=new Scanner(System.in);
+        String st=sc.nextLine();
+        System.out.println(st);
+        
+    }
+}
